1f80879e4c606e06d6cd9f2f129dd89a01ac4ee0
imanuelfebie <imanuel.febie@binus.ac.id>
2020-06-22 03:52:18 +0700

    Added API links

--- app/courses/api/routes.py
+++ app/courses/api/routes.py
@@ -1,12 +1,20 @@
 from app.courses.api.schema import course_schemas, class_code_schemas
-from app.courses.models import Course
+from app.courses.models import Course, ClassCode
 
 from flask import Blueprint, jsonify
 
 course_api = Blueprint('course_api', __name__)
 
 @course_api.route('/api/classes', methods=['GET'])
-def course_all_get():
+def class_list():
+    class_clode_list = ClassCode.query.all()
+    result = class_code_schemas.dump(class_clode_list)
+
+    return jsonify(result)
+    
+
+@course_api.route('/api/courses', methods=['GET'])
+def course_list():
     course_list = Course.query.all()
     result = course_schemas.dump(course_list)
 
